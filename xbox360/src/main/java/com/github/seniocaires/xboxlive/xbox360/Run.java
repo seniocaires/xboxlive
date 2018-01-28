d0230861e8259cd2a4ae51ac9b5dea199a000b6b
Senio Caires <seniocaires@gmail.com>
2018-01-28 00:36:54 -0200

    Escopo xbox360
    Estrutura inicial do crawler xbox360.

---
+++ xbox360/src/main/java/com/github/seniocaires/xboxlive/xbox360/Run.java
@@ -0,0 +1,12 @@
+package com.github.seniocaires.xboxlive.xbox360;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Run {
+
+	public static void main(String[] args) {
+		Logger.getLogger(Run.class.getSimpleName()).log(Level.INFO, "Iniciando Xbox360");
+	}
+
+}
